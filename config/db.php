7bacac763004db7253987c0b0b9e181972ffeba2
supergithubo <winston.los.santos@gmail.com>
2015-04-13 23:20:43 +0800

    Setup db config

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=vine',
     'username' => 'root',
-    'password' => '',
+    'password' => 'projectgreen',
     'charset' => 'utf8',
 ];
